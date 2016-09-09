64ca23fb5d61382fdd98ee8d487a48aea7afa8da
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-09-09 15:17:27 +0300

    Randomize size of shape

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ function createShape() {
     
     var positionLeftRandom = Math.random() * 400;
     
+    var widthRandom = Math.random() * 300 + 50;
+    
     document.getElementById("shape").style.top = positionTopRandom + "px";
     
     document.getElementById("shape").style.left = positionLeftRandom + "px";
+    
+    document.getElementById("shape").style.width = widthRandom + "px";
+    
+    document.getElementById("shape").style.height = widthRandom + "px";
     
     document.getElementById("shape").style.display = "block";
     
