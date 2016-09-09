f77cd7280f3451f22ebf6e0dbc3542ce16386044
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-09-09 14:20:47 +0300

    Setup timer to measure how long user takes to click the shape

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var start = new Date().getTime();
+
 document.getElementById("shape").onclick = function() {
     
     document.getElementById("shape").style.display = "none";
+    
+    var end = new Date().getTime();
+    
+    var timeTaken = (end - start) / 1000;
+    
+    alert(timeTaken);
     
 }
